29bdbb0253330d40c91c9b0da62cf11dff2e4cc2
qingnan.kong <bellakongqn@gmail.com>
2019-07-17 13:38:16 +0800

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route,Link } from 'react-router-dom';
+import Chat from './HigerOrderComponent/Chat';
+import DragDemo from './DragNew/DragDemo';
+import Facc from './Facc/Index';
+import ContextApi from './ContextApi/ContextApi';
+import ChangeTheme from './ChangeTheme/ChangeTheme'
+import PureRedux from './Redux/PureRedux';
+import Counter from './Counter/Counter';
+import ReduxThunk from './ReduxThunk/ReduxThunk';
+import About from './Page/About';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <div>
+            <ul>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/chat">Chat</Link></li>
+              <li>
+              <Link to="/drag">drag</Link>
+              </li>
+              
+              <li>
+                <Link to="/facc">FACC</Link>
+              </li>
+              <li>
+                <Link to="/contextApi">ContextApi</Link>
+              </li>
+              <li>
+                 <Link to="changeTheme">ChangeTheme</Link>
+              </li>
+              <li>
+                <Link to="pureRedux">PureRedux</Link>
+              </li>
+              <li>
+                <Link to="counter">Counter</Link>
+              </li>
+              <li>
+              <Link to="reduxThunk">ReduxThunk</Link>
+              </li>
+                
+                
+               
+              
+            </ul>
+            <div>
+              <Route path="/chat" render={() => <Chat/>} />
+              <Route path="/drag" render={() => <DragDemo/>}/>
+              <Route path="/about" render={() => <About/>} />
+              <Route path="/facc" render={() => <Facc/>}/>
+              <Route path="/contextApi"  render={() => <ContextApi/>} />
+              <Route path="/changeTheme" render={() => <ChangeTheme/>} />
+              <Route path="/pureRedux" render={() => <PureRedux/>} />
+              <Route path="/counter" render={() => <Counter/>}  />
+              <Route path="/reduxThunk" render={() => <ReduxThunk/>}  />
+            </div>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Drag from '../src/DragNew/DragDemo';
-import Chat from './HigerOrderComponent/Chat'
+// import Chat from './HigerOrderComponent/Chat'
 import Facc from './Facc/Index'
 import ContextApi from './ContextApi/ContextApi'
 import ChangeTheme from './ChangeTheme/ChangeTheme'
@@ -12,7 +12,7 @@ import Counter from './Counter/Counter';
 import ReduxThunk from './ReduxThunk/ReduxThunk'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<ReduxThunk />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
